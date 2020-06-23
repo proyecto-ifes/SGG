84437b2bffa5883f12354b2ad2210118ddf68cc8
Gabriela Alcantu <gaby.alcantu.90@live.com.ar>
2020-06-23 18:58:15 -0300

    Se crea el repositorio de la clase Socio asociado a la issue #1

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/socio/SocioRepository.java
@@ -0,0 +1,54 @@
+package domainapp.modules.simple.dom.impl.socio;
+
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
+import org.apache.isis.applib.services.registry.ServiceRegistry2;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.joda.time.LocalDate;
+
+import javax.inject.Inject;
+
+@DomainService(nature = NatureOfService.DOMAIN, repositoryFor = Socio.class)
+public class SocioRepository {
+
+    public Socio create(
+            final String nombre,
+            final String apellido,
+            final Integer dni,
+            final Integer telefono,
+            final String direccion,
+            final LocalDate fechaNac,
+            final Integer estado,
+            final String historiaClinica,
+            final Integer nroEmergencia,
+            final Integer peso,
+            final Integer altura,
+            final Boolean asistencia
+    ) {
+        final Socio object = new Socio(
+                nombre,
+                apellido,
+                dni,
+                telefono,
+                direccion,
+                fechaNac,
+                estado,
+                historiaClinica,
+                nroEmergencia,
+                peso,
+                altura,
+                asistencia);
+        serviceRegistry.injectServicesInto(object);
+        repositoryService.persist(object);
+        return object;
+    }
+
+    @Inject
+    RepositoryService repositoryService;
+
+    @Inject
+    ServiceRegistry2 serviceRegistry;
+    @javax.inject.Inject
+    IsisJdoSupport isisJdoSupport;
+}
