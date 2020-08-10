cc04e2e3410afecb1b505646c94d9011f3a9936e
Gabriela Alcantu <gaby.alcantu.90@live.com.ar>
2020-08-10 15:10:37 -0300

    Se modifica repositorio de Profesor para busqueda, asociado a la issue #15

--- module-simple/src/main/java/domainapp/modules/simple/dom/impl/profesor/ProfesorRepository.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/impl/profesor/ProfesorRepository.java
@@ -3,10 +3,14 @@ package domainapp.modules.simple.dom.impl.profesor;
 import domainapp.modules.simple.dom.impl.enums.Estado;
 import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.Programmatic;
+import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
 import org.apache.isis.applib.services.repository.RepositoryService;
+import org.datanucleus.query.typesafe.TypesafeQuery;
 import org.joda.time.LocalDate;
 
 import javax.inject.Inject;
+import java.util.List;
 
 @DomainService(nature = NatureOfService.DOMAIN, repositoryFor = Profesor.class)
 public class ProfesorRepository {
@@ -35,10 +39,36 @@ public class ProfesorRepository {
         return object;
     }
 
+    @Programmatic
+    public List<Profesor> findByApellido(
+            final String apellido
+    ) {
+        TypesafeQuery<Profesor> q = isisJdoSupport.newTypesafeQuery(Profesor.class);
+        final QProfesor cand = QProfesor.candidate();
+        q = q.filter(
+                cand.apellido.indexOf(q.stringParameter("apellido")).ne(-1)
+        );
+        return q.setParameter("apellido", apellido)
+                .executeList();
+    }
+
+    @Programmatic
+    public List<Profesor> findByDni(
+            final Integer dni
+    ) {
+        TypesafeQuery<Profesor> q = isisJdoSupport.newTypesafeQuery(Profesor.class);
+        final QProfesor cand = QProfesor.candidate();
+        q = q.filter(
+                cand.dni.eq(q.integerParameter("dni"))
+        );
+        return q.setParameter("dni", dni)
+                .executeList();
+    }
 
     @Inject
     RepositoryService repositoryService;
 
-
+    @javax.inject.Inject
+    IsisJdoSupport isisJdoSupport;
 
 }
