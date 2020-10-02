b1bcd90197d9dc0cada054d0c50a928af001586f
Gabriela Alcantu <gaby.alcantu.90@live.com.ar>
2020-10-01 21:21:17 -0300

    ISSUE #36: Mejoras Esteticas Se modifican estilos y scripts

--- webapp/src/main/webapp/scripts/application.js
+++ webapp/src/main/webapp/scripts/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-	/// here...
+
+	if ($("body").find(".accountManagementPanel").length > 0){
+
+        $("form:not(.filter) :input:visible:enabled").eq(0).attr("placeholder", "Ingrese nombre de usuario");
+        $("form:not(.filter) :input:visible:enabled").eq(1).attr("placeholder", "Ingrese contraseña");
+
+        $("button.btn[type=submit]").addClass("btn");
+
+        $('h1').css('margin-top', '-80px');
+        $('h2').css('margin-top', '-40px');
+
+    	}
 });
