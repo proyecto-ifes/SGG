a93840fe77e747b722496b7eadf3f374c1f499cd
Nicolas <niquitoalexis@gmail.com>
2020-11-13 16:41:31 -0300

    ISSUE #39: Modificacion para el componente Socio se modifica la vista principal y las rutas para la llamada al componente Socio

--- SGG-App/src/app/app-routing.module.ts
+++ SGG-App/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'socio/socio',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'socio',
+    loadChildren: () => import('./modules/socio/socio.module').then( m => m.SocioModule)
   }
 ];
 
--- SGG-App/src/app/app.component.ts
+++ SGG-App/src/app/app.component.ts
@@ -13,14 +13,14 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Mis Datos',
+      url: '/socio/socio',
+      icon: 'person'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
+      title: 'Mis Rutinas',
+      url: '/socio/rutinas',
+      icon: 'clipboard'
     },
     {
       title: 'Favorites',
@@ -31,19 +31,8 @@ export class AppComponent implements OnInit {
       title: 'Archived',
       url: '/folder/Archived',
       icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
   constructor(
     private platform: Platform,
@@ -61,9 +50,9 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
-    }
+    // const path = window.location.pathname.split('socio/')[1];
+    // if (path !== undefined) {
+    //   this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    // }
   }
 }
